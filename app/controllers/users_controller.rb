5754bd57a99dc0f9159e5c590ab8bb6312020a31
cmutty <cmutty7@gmail.com>
2013-06-11 14:13:01 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+  	 @user = User.new
+  end
+ def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to asonios!"
+      redirect_to @user
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+ def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
