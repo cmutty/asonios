dad2daae5b06e760c9a80afb297c3050fd7c167c
cmutty <cmutty7@gmail.com>
2013-06-10 20:14:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asonios::Application.config.secret_token = 'd0607ea6f49458c03815197638f3f0e51c25b8178dca053833e29d180cd964c0079f4c19f0eed32dabe9f9208fa4d0f12ddcfe97cbe71e52fb1e87f031e0c7ff'
